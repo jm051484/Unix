b85c271d68c24c98c28eb123d47fd62b06a72057
0001username <dexter09471@gmail.com>
2018-06-13 22:34:42 +0800

    Update serverUninstall.sh

--- serverUninstall.sh
+++ serverUninstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Unsinstalling..."
 cd ..
 /etc/init.d/vpnserver stop
 rm -r /etc/init.d/vpnserver
@@ -7,3 +8,6 @@ rm -r soft*
 rm /etc/resolv.conf && cp /etc/resolv.confx /etc/resolv.conf
 rm /etc/sysctl.conf && cp /etc/sysctl.confx /etc/sysctl.conf
 rm /root/serverUninstall.sh*
+clear
+echo "[ Setup Finished ]"
+echo "AutoScript By: Dexter Cellona Banawon (PHC - Granade)"
