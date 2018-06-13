3b5ebab6909b17b7be1e92838b1978703337ea4d
0001username <dexter09471@gmail.com>
2018-06-13 22:23:31 +0800

    Delete serverUninstall.sh

--- serverUninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-/etc/init.d/vpnserver stop
-rm -r /etc/init.d/vpnserver
-rm -r /usr/local/vpnserver
-rm -r soft*
-rm /etc/resolv.conf && cp /etc/resolv.confx /etc/resolv.conf
-rm /etc/sysctl.conf && cp /etc/sysctl.confx /etc/sysctl.conf
