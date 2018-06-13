f41f69a86a5b459703b99ae63def1813296ca695
0001username <dexter09471@gmail.com>
2018-06-13 22:06:43 +0800

    Update serverUpdater.sh

--- serverUpdater.sh
+++ serverUpdater.sh
@@ -3,12 +3,12 @@ echo "Stopping the server..."
 /etc/init.d/vpnserver stop
 echo "Recovering server config..."
 cd .. && cp -r /usr/local/vpnserver/*vpn_server.config /root/serverBackup/
-rm -rf /etc/init.d/vpnserver
-rm -rf /usr/local/vpnserver
+rm -r /etc/init.d/vpnserver
+rm -r /usr/local/vpnserver
 echo "Downloading the latest package..."
 wget http://www.softether-download.com/files/softether/v4.27-9668-beta-2018.05.29-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.27-9668-beta-2018.05.29-linux-x64-64bit.tar.gz
 tar -xzf softether*
-rm -rf softether*
+rm -r softether*
 echo "Installing the latest package..."
 cd vpnserver && expect -c 'spawn make; expect number:; send 1\r; expect number:; send 1\r; expect number:; send 1\r; interact'
 cd .. && mv vpnserver /usr/local && chmod 600 * /usr/local/vpnserver/ && chmod 700 /usr/local/vpnserver/vpncmd && chmod 700 /usr/local/vpnserver/vpnserver
@@ -66,5 +66,5 @@ echo "Starting the server..."
 sleep 5
 clear
 cd root
-rm serverUpdater.sh*
 echo "AutoScript By: Dexter Cellona Banawon (PHC - Granade)"
+rm -r serverUpdater.sh*
