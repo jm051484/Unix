1df92bfadc05adde3c5554e54a7d57037ddc68d7
Dexter Cellona Banawon <dexter09471@gmail.com>
2018-06-18 10:23:45 +0800

    Delete pre-Setup.sh

--- ubuntu-debian/pre-Setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Updating the system first..."
-apt-get -y update && apt-get -y upgrade && apt-get install expect -y
-apt-get install checkinstall build-essential -y
-wget https://raw.githubusercontent.com/X-DCB/SE_Scripts/master/ubuntu-debian/serverSetup && chmod +x serverSetup && ./serverSetup
