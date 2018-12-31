b5d8bfe0d870dda7e83d1f3a28d4036f6f4a54e3
Dexter Cellona Banawon <dexter09471@gmail.com>
2018-12-31 19:41:23 +0800

    Add files via upload

--- openvpn/resetDatabase.sh
+++ openvpn/resetDatabase.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Delete all usage and users
+mysqldump openvpn > db_saved.sql
 mysql openvpn -e "delete from log;delete from user"
 # Restore saved database
-cat db_saved.sql | mysql openvpn
+# cat db_saved.sql | mysql openvpn
