cdb772ac0ceb585c4b0df8d6364c8348ffe8dd58
Dexter Cellona Banawon <dexter09471@gmail.com>
2019-01-19 02:45:33 +0800

    Add files via upload

--- openvpn/anti-ddos.sh
+++ openvpn/anti-ddos.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
-gitlink1='https://raw.githubusercontent.com/jgmdev/ddos-deflate/master/'
-function getfile {
-	[ ! -d $1 ] && mkdir -R $1
-	if[ $4 = 1 ];then
-		wget -qO $1$2 $gitlink'/src/'$3
-	else wget -q0 $1$2 $gitlink'/config/'$3 ;fi
-	chmod a+wxr $1$2
+gitlink='https://raw.githubusercontent.com/jgmdev/ddos-deflate/master/'
+function addfile {
+	[ -d $2 ] || mkdir -p $2
+	if [ $1 = 1 ];then
+		wget -qO $2$3 $gitlink'src/'$4
+	else wget -qO $2$3 $gitlink'config/'$4 ;fi
+	chmod -R a+wxr $2$3
 }
-getfile /sbin/ ddos ddos.sh 1
-getfile /etc/systemd/system/ ddos.service ddos.service 1
-getfile /etc/rc.d/ ddos ddos.rcd 1
-getfile /etc/newsyslog.conf.d/ ddos ddos.newsyslog 1
-getfile /etc/logrotate.d/ ddos ddos.logrotate 1
-getfile /etc/init.d/ ddos ddos.initd 1
-getfile /etc/ddos/ ddos.conf ddos.conf 0
-getfile /etc/ddos/ ignore.host.list ignore.host.list 0
-getfile /etc/ddos/ ignore.ip.list ignore.ip.list 0
+addfile 1 /sbin/ ddos ddos.sh
+addfile 1 /etc/systemd/system/ ddos.service ddos.service
+addfile 1 /etc/rc.d/ ddos ddos.rcd
+addfile 1 /etc/newsyslog.conf.d/ ddos ddos.newsyslog
+addfile 1 /etc/logrotate.d/ ddos ddos.logrotate
+addfile 1 /etc/init.d/ ddos ddos.initd
+addfile 0 /etc/ddos/ ddos.conf ddos.conf
+addfile 0 /etc/ddos/ ignore.host.list ignore.host.list
+addfile 0 /etc/ddos/ ignore.ip.list ignore.ip.list
+systemctl daemon-reload
+systemctl enable ddos
+systemctl start ddos
+ddos --cron
+clear
+wget -qO- "https://raw.githubusercontent.com/X-DCB/Unix/master/banner" | bash
+echo "This is only a test and will official be if it's really working."
+echo "Installation finished."
+history -c
