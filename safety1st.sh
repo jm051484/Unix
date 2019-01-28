c10e58c0ae1a3417669818ff9b7a57a4d4d589c6
Dexter Cellona Banawon <dexter09471@gmail.com>
2019-01-28 17:58:13 +0800

    Add files via upload

--- safety1st.sh
+++ safety1st.sh
@@ -69,6 +69,7 @@ cd /etc/openvpn
 if ! [ -d easy-rsa ];then
 wget -qO- https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.4/EasyRSA-3.0.4.tgz | tar xz
 mv EasyRSA-3.0.4 easy-rsa || return
+cd easy-rsa
 SERVER_CN="cn_$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 16 | head -n 1)"
 SERVER_NAME="server_$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 16 | head -n 1)"
 echo "set_var EASYRSA_KEY_SIZE $RSA_KEY_SIZE
