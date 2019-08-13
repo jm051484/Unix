50a7f354096abe94ada10bb9cdbd87d442b42dc2
Dexter Cellona Banawon <28706220+X-DCB@users.noreply.github.com>
2019-08-13 18:49:05 +0800

    Add files via upload

--- stunnel.sh
+++ stunnel.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 if [ "$(id -u)" -ne 0 ]; then
 	echo -ne "\nPlease execute this script as root.\n"
 	exit 1; fi
@@ -11,5 +10,9 @@ export DEBIAN_FRONTEND=noninteractive
 apt install -y stunnel4
 wget https://github.com/X-DCB/Unix/raw/master/stunnel.tar.gz -qO- | tar xz -C /etc/stunnel
 
+systemctl restart stunnel4
+
 echo -ne "\nStunnel installed.\n
-Script by Dexter Cellona Banawon"
+Script by Dexter Cellona Banawon\n"
+
+exit 0
